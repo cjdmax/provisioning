1438c2a15aec47ce1c37029e809c06edfad38421
Liam Macgillavry <liam@kumina.nl>
2017-05-22 00:08:53 +0200

    changed download URL

--- service/etcd/scripts/install.sh
+++ service/etcd/scripts/install.sh
@@ -4,6 +4,6 @@ set -e
 rm -f /opt/etcd-${version}-linux-amd64.tar.gz
 rm -rf /opt/etcd && mkdir -p /opt/etcd
 
-curl -L https://storage.googleapis.com/etcd/${version}/etcd-${version}-linux-amd64.tar.gz \
+curl -L https://github.com/coreos/etcd/releases/download/${version}/etcd-${version}-linux-amd64.tar.gz \
   -o /opt/etcd-${version}-linux-amd64.tar.gz
 tar xzvf /opt/etcd-${version}-linux-amd64.tar.gz -C /opt/etcd --strip-components=1
